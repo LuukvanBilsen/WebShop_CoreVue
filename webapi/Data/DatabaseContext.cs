1333df05a3dd8b01a522756b17ba8ee9f8abe6bd
Luuk van Bilsen <luuk.vanbilsen1@gmail.com>
2023-03-27 22:48:35 +0200

    Added databaseContext

---
+++ webapi/Data/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.Models;
+
+namespace webapi.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        {
+
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=webshopdb;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
